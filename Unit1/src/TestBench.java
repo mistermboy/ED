5a142e72f1fe9f241605e08a48c17e1aa7dc9d4c
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-09-21 18:46:30 +0200

    Arreglos por culpa de willy

--- Unit1/src/TestBench.java
+++ Unit1/src/TestBench.java
@@ -16,11 +16,10 @@ public class TestBench {
 		t.testLogaritmic("SalidaLogaritmic", 0, 5, 5);
 	}
 	
-	// TUS MUUUELAS
 
 	
 	/**
-	 * Mï¿½todo que duerme el hilo durante el tiempo que se le pasa por constante	SLLEP_TIME
+	 * Método que duerme el hilo durante el tiempo que se le pasa por constante	SLLEP_TIME
 	 * 
 	 */
 	private static void doNothing() {
@@ -32,8 +31,8 @@ public class TestBench {
 	}
 	
 	/**
-	 *  Mï¿½todo que realiza un algoritmo con complejidad O(n)
-	 * @param n, nï¿½mero de veces que se repite el algoritmo, de tipo int
+	 *  Método que realiza un algoritmo con complejidad O(n)
+	 * @param n, número de veces que se repite el algoritmo, de tipo int
 	 */
 	public static void linear(int n) {
 		
@@ -44,8 +43,8 @@ public class TestBench {
 	}
 	 
 	 /**
-	  * Mï¿½todo que realiza un algoritmo con complejidad O(n^2)
-	  * @param n, nï¿½mero de veces que se repite el algoritmo, de tipo int
+	  * Método que realiza un algoritmo con complejidad O(n^2)
+	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void quadratic(int n) {
 
@@ -57,8 +56,8 @@ public class TestBench {
 	}
 	 
 	 /**
-	  * Mï¿½todo que realiza un algoritmo  con complejidad O(n^3)
-	  * @param n, nï¿½mero de veces que se repite el algoritmo, de tipo int
+	  * Método que realiza un algoritmo  con complejidad O(n^3)
+	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void cubic(int n) {
 
@@ -72,8 +71,8 @@ public class TestBench {
 	}
 
 	 /**
-	  * Mï¿½todo que realiza un algoritmo con complejidad O(log n)
-	  * @param n, nï¿½mero de veces que se repite el algoritmo, de tipo int
+	  * Método que realiza un algoritmo con complejidad O(log n)
+	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void logarithm(int n) {
 
@@ -85,9 +84,9 @@ public class TestBench {
 
 
 	/**
-	 * Mï¿½todo que relaiza un algoritmo que eleva 2 a el nï¿½mero que se le pasa por parï¿½metro.
+	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parï¿½metro.
 	 * La complejidad es lineal, ya que solo tiene un bucle for.
-	 * @param n, numï¿½ro al que se le quiere elevar el 2, de tipo int 
+	 * @param n, número al que se le quiere elevar el 2, de tipo int 
 	 */		 
 	public static long pow2iter(int n) {
 		long total = 1;
@@ -101,7 +100,7 @@ public class TestBench {
 	}
 
 	/**
-	 * Mï¿½todo que estudia el comportamiento del algoritmo con complejidad
+	 * Método que estudia el comportamiento del algoritmo con complejidad
 	 * lineal.
 	 * 
 	 * @param output
@@ -111,7 +110,7 @@ public class TestBench {
 	 * @param endN
 	 *            Cota superior, de tipo int
 	 * @param times
-	 *            Nï¿½mero de veces que se ejecuta el algotitmo por iteraciï¿½n, de tipo int
+	 *            Número de veces que se ejecuta el algotitmo por iteración, de tipo int
 	 */
 	public void testLinear(String output, int startN, int endN, int times) {
 
@@ -145,8 +144,8 @@ public class TestBench {
 	}
 
 	/**
-	 * Mï¿½todo que estudia el comportamiento del algoritmo con complejidad
-	 * cuadrï¿½tica.
+	 * Método que todo que estudia el comportamiento del algoritmo con complejidad
+	 * cuadrática.
 	 * 
 	  * @param output
 	 *            Nombre del fichero de salida, te tipo csv, de tipo String
@@ -155,7 +154,7 @@ public class TestBench {
 	 * @param endN
 	 *            Cota superior, de tipo int
 	 * @param times
-	 *            Nï¿½mero de veces que se ejecuta el algotitmo por iteraciï¿½n, de tipo int
+	 *            Número de veces que se ejecuta el algotitmo por iteración, de tipo int
 	 */
 	public void testQuadratic(String output, int startN, int endN, int times) {
 
@@ -191,8 +190,8 @@ public class TestBench {
 	
 
 	/**
-	 * Mï¿½todo que estudia el comportamiento del algoritmo con complejidad
-	 * cï¿½bica.
+	 * Método que todo que estudia el comportamiento del algoritmo con complejidad
+	 * cúbica.
 	 * 
 	  * @param output
 	 *            Nombre del fichero de salida, te tipo csv, de tipo String
@@ -201,7 +200,7 @@ public class TestBench {
 	 * @param endN
 	 *            Cota superior, de tipo int
 	 * @param times
-	 *            Nï¿½mero de veces que se ejecuta el algotitmo por iteraciï¿½n, de tipo int
+	 *            Número de veces que se ejecuta el algotitmo por iteración, de tipo int
 	 */
 	public void testCubic(String output, int startN, int endN, int times) {
 
@@ -236,8 +235,8 @@ public class TestBench {
 	
 	
 	/**
-	 * Mï¿½todo que estudia el comportamiento del algoritmo con complejidad
-	 * logarï¿½tmica.
+	 * Método que todo que estudia el comportamiento del algoritmo con complejidad
+	 * logarítmica.
 	 * 
 	  * @param output
 	 *            Nombre del fichero de salida, te tipo csv, de tipo String
@@ -246,7 +245,7 @@ public class TestBench {
 	 * @param endN
 	 *            Cota superior, de tipo int
 	 * @param times
-	 *            Nï¿½mero de veces que se ejecuta el algotitmo por iteraciï¿½n, de tipo int
+	 *            Número de veces que se ejecuta el algotitmo por iteración, de tipo int
 	 */
 	public void testLogaritmic(String output, int startN, int endN, int times) {
 
