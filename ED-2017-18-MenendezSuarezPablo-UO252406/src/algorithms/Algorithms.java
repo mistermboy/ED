aecf19d3ca24c80b374c1f7c2e0be84a187fb630
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-10-01 17:54:44 +0200

    Finalizado

--- ED-2017-18-MenendezSuarezPablo-UO252406/src/algorithms/Algorithms.java
+++ ED-2017-18-MenendezSuarezPablo-UO252406/src/algorithms/Algorithms.java
@@ -23,6 +23,9 @@ public class Algorithms {
 	 * @param n, número de veces que se repite el algoritmo, de tipo int
 	 */
 	public static void linear(int n) {
+		if (n < 0) {
+			return;
+		}
 		
 		for (int i = 0; i <= n; i++) {
 
@@ -35,6 +38,9 @@ public class Algorithms {
 	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void quadratic(int n) {
+		if (n < 0) {
+			return;
+		}
 
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= n; j++) {
@@ -48,6 +54,10 @@ public class Algorithms {
 	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void cubic(int n) {
+
+		if (n < 0) {
+			return;
+		}
 
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= n; j++) {
@@ -63,7 +73,10 @@ public class Algorithms {
 	  * @param n, número de veces que se repite el algoritmo, de tipo int
 	  */
 	public static void logarithm(int n) {
-		for (int i = 1; i <= n; i*=2) {
+		if (n < 0) {
+			return;
+		}
+		for (int i = 1; i <= n; i *= 2) {
 			doNothing();
 		}
 
@@ -74,8 +87,12 @@ public class Algorithms {
 	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parï¿½metro.
 	 * La complejidad es lineal, ya que solo tiene un bucle for.
 	 * @param n, número al que se le quiere elevar el 2, de tipo int 
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */		 
 	public static long pow2iter(int n) {
+		if (n < 0) {
+			return -1;
+		}
 		long total = 1;
 		if (n > 0)
 			for (int i = 0; i < n; i++) {
@@ -90,10 +107,15 @@ public class Algorithms {
 	 * Método iterativo que calcula el factorial de un número que se le pasa por parámetro
 	 * @param n número del que se desea averiguar su factorial, de tipo int
 	 * @return factorial del número indicado, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */
-	public static long factorial(int n) {
+	public  long factorialIterativo(int n) {
+		if (n < 0) {
+			return -1;
+		}
 		long aux = 1;
 		for (int i = n; i > 0; i--) {
+			doNothing();
 			aux *= i;
 		}
 
@@ -105,8 +127,12 @@ public class Algorithms {
 	 * Método recursivo que calcula el factorial de un número que se le pasa por parámetro
 	 * @param n número del que se desea averiguar su factorial, de tipo int
 	 * @return factorial del número indicado, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */
-	public static long factorialRec(int n) {
+	public  long factorialRec(int n) {
+		if (n < 0) {
+			return -1;
+		}
 		doNothing();
 		if (n == 0) {
 			return 1;
@@ -121,23 +147,27 @@ public class Algorithms {
 	 * Método iterativo que calcula el término de la sucesión de fibonacci que se le indica
 	 * @param n termino de la sucesión que se desea averiguar, de tipo int
 	 * @return resultado, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */
-	public static long fibonacci(int n) {
-		long aux=0;
-		doNothing();
+	public  long fibonacciIterativo(int n) {
+		if (n < 0) {
+			return -1;
+		}
+		long n1=0;
+		long n2=1;
 		if(n==0) {
 			return 0;
 		}else if(n==1) {
 			return 1;
-		}else if(n==2) {
-			return 1;
 		}
 		else {
-			for (int i = 3; i <= n; i++) {
-				aux += n-1;
+			for (int i = 0; i <= n-2; i++) {
+				doNothing();
+				 n2 = n1 + n2;
+		         n1 = n2 - n1;
 			}
 		}
-		return aux;
+		return n2;
 	}
 	
 	
@@ -145,17 +175,21 @@ public class Algorithms {
 	 * Método recursivo que calcula el término de la sucesión de fibonacci que se le indica
 	 * @param n termino de la sucesión que se desea averiguar, de tipo int
 	 * @return resultado, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */
-	public static long fibonacciRec(int n) {
-		doNothing();
-		if(n==0) {
-			return 0;
-		}else if(n==1) {
-			return 1;
-		}else {
-			return  fibonacciRec(n-1) + fibonacciRec(n-2);
+	public long fibonacciRec(int n) {
+		if (n >= 0) {
+			doNothing();
+			if (n == 0) {
+				return 0;
+			} else if (n == 1) {
+				return 1;
+			} else {
+				return fibonacciRec(n - 1) + fibonacciRec(n - 2);
+			}
 		}
-		
+		return -1;
+
 	}
 	
 
@@ -165,63 +199,84 @@ public class Algorithms {
 	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parámetro. 
 	 * La complejidad es lineal.
 	 * @param n, numéro al que se le quiere elevar el 2, de tipo int 
+	 * * @return el resulado obtenido, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */	
-	public long pow1(int n) {
-		doNothing();
-		if (n == 0) {
-			return 1;
-		} else {
-			return 2 * pow1(n - 1);
+	public long pow1Rec(int n) {
+		if (n >= 0) {
+			doNothing();
+			if (n == 0) {
+				return 1;
+			} else {
+				return 2 * pow1Rec(n - 1);
+			}
 		}
+		return -1;
 	}
-	
+
 	/**
 	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parámetro. 
 	 * La complejidad es cúbica.
 	 * @param n, numéro al que se le quiere elevar el 2, de tipo int
+	 * @return el resulado obtenido, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */	
-	public long pow2(int n) {
-		doNothing();
-		if (n == 0) {
-			return 1;
-		} else {
-			return pow2(n - 1) + pow2(n - 1);
+	public long pow2Rec(int n) {
+		if (n >= 0) {
+			doNothing();
+			if (n == 0) {
+				return 1;
+			} else {
+				return pow2Rec(n - 1) + pow2Rec(n - 1);
+			}
 		}
+		return -1;
 	}
 	
 	/**
 	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parámetro. 
 	 * La complejidad es logarítmica.
 	 * @param n, numéro al que se le quiere elevar el 2, de tipo int
+	 * @return el resulado obtenido, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */	
-	public long pow3(int n) {
-		doNothing();
-		if (n == 0) {
-			return 1;
-		} else if (n % 2 == 0) {
-			return pow3(n / 2) * pow3(n / 2);
-		} else {
-			return pow3(n / 2) * pow3(n / 2) * 2;
+	public long pow3Rec(int n) {
+		if (n >= 0) {
+
+			doNothing();
+			if (n == 0) {
+				return 1;
+			} else if (n % 2 == 0) {
+				return pow3Rec(n / 2) * pow3Rec(n / 2);
+			} else {
+				return pow3Rec(n / 2) * pow3Rec(n / 2) * 2;
+			}
 		}
+		return -1;
 	}
 
 	/**
 	 * Método que relaiza un algoritmo que eleva 2 a el número que se le pasa por parámetro. 
 	 * La complejidad es lineal.
 	 * @param n, numéro al que se le quiere elevar el 2, de tipo int
+	 * @return el resulado obtenido, de tipo long
+	 * 			-1 en caso de que el n introducido sea menor que 0
 	 */	
-	public static long pow4(int n) {
-		doNothing();
-		if (n == 0) {
-			return 1;
-		} else {
-			long aux = pow4(n / 2);
-			aux = aux * aux;
-			if (n % 2 != 0)
-				return aux * 2;
-			else
-				return aux;
+	public static long pow4Rec(int n) {
+		if (n >= 0) {
+			doNothing();
+			if (n == 0) {
+				return 1;
+			} else {
+				long aux = pow4Rec(n / 2);
+				aux = aux * aux;
+				if (n % 2 != 0)
+					return aux * 2;
+				else
+					return aux;
+			}
 		}
+		return -1;
 	}
 	
 	
--- ED-2017-18-MenendezSuarezPablo-UO252406/src/test/AlgorithmsTest.java
+++ ED-2017-18-MenendezSuarezPablo-UO252406/src/test/AlgorithmsTest.java
@@ -11,61 +11,145 @@ public class AlgorithmsTest {
 	
 	Algorithms a = new Algorithms();
 	
-
 	
 	@Test
-	public void testFactorial() {
-		assertEquals(24,a.factorial(4));
-		assertEquals(1,a.factorial(0));
+	public void testFactorialIterativo() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.factorialIterativo(0));
+		assertEquals(1,a.factorialIterativo(1));
+		assertEquals(2,a.factorialIterativo(2));
+		assertEquals(6,a.factorialIterativo(3));
+		assertEquals(24,a.factorialIterativo(4));
+		assertEquals(120,a.factorialIterativo(5));
+		assertEquals(720,a.factorialIterativo(6));
+		assertEquals(5040,a.factorialIterativo(7));
+		assertEquals(40320,a.factorialIterativo(8));
+		assertEquals(362880,a.factorialIterativo(9));
+		assertEquals(3628800,a.factorialIterativo(10));
+		assertEquals(39916800,a.factorialIterativo(11));
+		assertEquals(479001600,a.factorialIterativo(12));
+		
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.factorialIterativo(-9));
+		
+		
 	}
 
-//	@Test
-//	public void testFactorialRec() {
-//		
-//	}
-//
-//	@Test
-//	public void testFibonacciRec() {
-//		
-//	}
-	
+	@Test
+	public void testFactorialRec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.factorialRec(0));
+		assertEquals(1,a.factorialRec(1));
+		assertEquals(2,a.factorialRec(2));
+		assertEquals(6,a.factorialRec(3));
+		assertEquals(24,a.factorialRec(4));
+		assertEquals(120,a.factorialRec(5));
+		assertEquals(720,a.factorialRec(6));
+		assertEquals(5040,a.factorialRec(7));
+		assertEquals(40320,a.factorialRec(8));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.factorialRec(-9));
+	}
+
+	@Test
+	public void testFibonacciRec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(0,a.fibonacciRec(0));
+		assertEquals(1,a.fibonacciRec(1));
+		assertEquals(1,a.fibonacciRec(2));
+		assertEquals(2,a.fibonacciRec(3));
+		assertEquals(3,a.fibonacciRec(4));
+		assertEquals(5,a.fibonacciRec(5));
+		assertEquals(8,a.fibonacciRec(6));
+		assertEquals(13,a.fibonacciRec(7));
+		assertEquals(21,a.fibonacciRec(8));
+		assertEquals(34,a.fibonacciRec(9));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.fibonacciRec(-9));
+	}
 	
 	@Test
-	public void testPow1() {
-		assertEquals(1,a.pow1(0));
-		assertEquals(2,a.pow1(1));
-		assertEquals(4,a.pow1(2));
-		assertEquals(8,a.pow1(3));
-		assertEquals(16,a.pow1(4));
+	public void testFibonacciIterativo() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(0,a.fibonacciIterativo(0));
+		assertEquals(1,a.fibonacciIterativo(1));
+		assertEquals(1,a.fibonacciIterativo(2));
+		assertEquals(2,a.fibonacciIterativo(3));
+		assertEquals(3,a.fibonacciIterativo(4));
+		assertEquals(5,a.fibonacciIterativo(5));
+		assertEquals(8,a.fibonacciIterativo(6));
+		assertEquals(13,a.fibonacciIterativo(7));
+		assertEquals(21,a.fibonacciIterativo(8));
+		assertEquals(34,a.fibonacciIterativo(9));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.fibonacciIterativo(-9));
 	}
 	
 	
 	@Test
-	public void testPow2() {
-		assertEquals(1,a.pow2(0));
-		assertEquals(2,a.pow2(1));
-		assertEquals(4,a.pow2(2));
-		assertEquals(8,a.pow2(3));
-		assertEquals(16,a.pow2(4));
+	public void testPow1Rec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.pow1Rec(0));
+		assertEquals(2,a.pow1Rec(1));
+		assertEquals(4,a.pow1Rec(2));
+		assertEquals(8,a.pow1Rec(3));
+		assertEquals(16,a.pow1Rec(4));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.pow1Rec(-9));
 	}
 	
 	
 	@Test
-	public void testPow3() {
-		assertEquals(1,a.pow3(0));
-		assertEquals(2,a.pow3(1));
-		assertEquals(4,a.pow3(2));
-		assertEquals(8,a.pow3(3));
-		assertEquals(16,a.pow3(4));
+	public void testPow2Rec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.pow2Rec(0));
+		assertEquals(2,a.pow2Rec(1));
+		assertEquals(4,a.pow2Rec(2));
+		assertEquals(8,a.pow2Rec(3));
+		assertEquals(16,a.pow2Rec(4));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.pow2Rec(-9));
+	}
+	
+	
+	@Test
+	public void testPow3Rec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.pow3Rec(0));
+		assertEquals(2,a.pow3Rec(1));
+		assertEquals(4,a.pow3Rec(2));
+		assertEquals(8,a.pow3Rec(3));
+		assertEquals(16,a.pow3Rec(4));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.pow3Rec(-9));
 	}
 	
 	@Test
-	public void testPow4() {
-		assertEquals(1,a.pow4(0));
-		assertEquals(2,a.pow4(1));
-		assertEquals(4,a.pow4(2));
-		assertEquals(8,a.pow4(3));
-		assertEquals(16,a.pow4(4));
+	public void testPow4Rec() {
+		
+		//PRUEBAS POSITIVAS
+		assertEquals(1,a.pow4Rec(0));
+		assertEquals(2,a.pow4Rec(1));
+		assertEquals(4,a.pow4Rec(2));
+		assertEquals(8,a.pow4Rec(3));
+		assertEquals(16,a.pow4Rec(4));
+		
+		//PRUEBAS NEGATIVAS
+		assertEquals(-1,a.pow4Rec(-9));
 	}
 	
 
